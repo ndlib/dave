aa404d91b3f33ff0e75e0c729ef5ac1c7e948f48
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-08-29 10:17:39 -0400

    Linting non-component files in `src`
    Using:
    https://atom.io/packages/linter-eslint
    https://atom.io/packages/linter-scss-lint
    https://atom.io/packages/react

--- src/modules/buildArtifactImage.js
+++ src/modules/buildArtifactImage.js
@@ -1,8 +1,8 @@
-function buildArtifactImage(data, params, offset = 0) {
-  var pageId = parseInt(params.pageId) + offset;
-  var sequence = parseInt(params.sequence);
-  var canvas = data.sequences[sequence].canvases[pageId];
-  var imageUri = canvas.images[0].resource['@id'];
+function buildArtifactImage (data, params, offset = 0) {
+  var pageId = parseInt(params.pageId) + offset
+  var sequence = parseInt(params.sequence)
+  var canvas = data.sequences[sequence].canvases[pageId]
+  var imageUri = canvas.images[0].resource['@id']
   return {
     pageId: pageId,
     alt: canvas.label,
@@ -10,4 +10,4 @@ function buildArtifactImage(data, params, offset = 0) {
   }
 }
 
-export default buildArtifactImage;
+export default buildArtifactImage
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import { routerReducer as router } from 'react-router-redux'
-import  manifest from '../routes/Manifest/'
+import manifest from '../routes/Manifest/'
 
 export const makeRootReducer = (asyncReducers) => {
   return combineReducers({
