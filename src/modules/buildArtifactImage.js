336c179a44156afd4b29cf25ec2365eaa75617c3
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-09-08 15:16:57 -0400

    Allow dependency injection of canvas and sequence in buildArtifactImage
    This lets the grid view specify exactly which image should be rendered
    independent of the URL params.
    
    WIP DAVE-24

--- src/modules/buildArtifactImage.js
+++ src/modules/buildArtifactImage.js
@@ -1,18 +1,26 @@
 // Build an object that contains all the data an ArtifactImage needs
-function buildArtifactImage (data, params, offset = 0) {
-  const pageId = parseInt(params.pageId) + offset
-  const sequence = parseInt(params.sequence)
-  const canvas = data.sequences[sequence].canvases[pageId]
-  const imageUri = canvas.images[0].resource['@id']
+function buildArtifactImage (
+  data,
+  params,
+  offset = 0,
+  canvas = null,
+  sequence = null
+) {
+  canvas === null ? canvas = params.pageId : true
+  sequence === null ? sequence = params.sequence : true
+  const canvasId = parseInt(canvas) + offset
+  const sequenceId = parseInt(sequence)
+  const canvasObject = data.sequences[sequenceId].canvases[canvasId]
+  const imageUri = canvasObject.images[0].resource['@id']
   const zoomLink = '/' + params.source +
               '/' + params.manifest +
               '/' + params.sequence +
               '/' + params.view +
-              '/' + pageId +
+              '/' + canvasId +
               '/detail'
   return {
-    pageId: pageId,
-    alt: canvas.label,
+    pageId: canvasId,
+    alt: canvasObject.label,
     imageUri: imageUri,
     zoomLink: zoomLink
   }
