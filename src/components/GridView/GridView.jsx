316d62e239bd1978655d30b35906619bdc712f75
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-09-08 10:41:09 -0400

    Simplifying render loop slightly
    WIP DAVE-24

--- src/components/GridView/GridView.jsx
+++ src/components/GridView/GridView.jsx
@@ -42,17 +42,16 @@ class GridView extends Component {
     const data = this.props.data
     const params = this.props.params
     const currentSequence = params.sequence * 1
-    const canvases = data.sequences[currentSequence].canvases
-    const canvasCount = canvases.length
+    const canvasCount = data.sequences[currentSequence].canvases.length
     const columnCount = this.numberOfColumns(this.state.windowWidth)
     const rowCount = Math.ceil(canvasCount / columnCount)
     // const currentCanvas = params.pageId * 1
-    let remainingCanvases = canvasCount
-    let gridRows = []
     let count = columnCount
-    let start = 0
+    let gridRows = []
     let i = 0
     for (i; i < rowCount; i++) {
+      let rowStart = i * columnCount
+      let remainingCanvases = (canvasCount - rowStart)
       if (remainingCanvases < columnCount) {
         count = remainingCanvases
       }
@@ -60,14 +59,12 @@ class GridView extends Component {
         <GridRow
           data={data}
           params={params}
-          start={start}
+          start={rowStart}
           key={i}
           maxColumns={columnCount}
           count={count}
         />
       )
-      remainingCanvases = remainingCanvases - columnCount
-      start = start + columnCount
     }
     return (
       <div className={classes.gridview}>
