8de4da5f1781f991ddf6988cb98173e347b1831e
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2016-09-22 15:07:24 -0400

    The URL validator expression does not handle some valid URLs

--- tests/components/ReferralButton/modules/getSourceUri.spec.js
+++ tests/components/ReferralButton/modules/getSourceUri.spec.js
@@ -2,8 +2,16 @@ import getSourceUri from 'components/ReferralButton/modules/getSourceUri'
 
 describe('(Module) getSourceUri', () => {
   it('Returns a valid url when passed a valid url', () => {
-    let _url = getSourceUri('?ref=http://google.com')
-    expect(_url).to.equal('http://google.com')
+    let validURLs = [
+      'https://google.com',
+      'https://www.nd.edu',
+      'http://nd.edu', // Fails
+      'https://osf.io' // Fails
+    ]
+    for (let _url of validURLs) {
+      let _source = getSourceUri('?ref=' + _url)
+      expect(_source).to.equal(_url)
+    }
   })
 
   it('Returns null when passed an invalid url', () => {
