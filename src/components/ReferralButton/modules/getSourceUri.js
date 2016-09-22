9b34a79c9ca46a74397ce27befdb3256c2a6c910
Dan Wolfe <dwolfe2@nd.edu>
2016-09-22 15:37:17 -0400

    Use library for uri validation

--- src/components/ReferralButton/modules/getSourceUri.js
+++ src/components/ReferralButton/modules/getSourceUri.js
@@ -1,16 +1,12 @@
-// Return true if the passed value is a validUri
-// http://stackoverflow.com/questions/8667070/javascript-regular-expression-to-validate-url
-function validUri (value) {
-  return /((ftp|https?):\/\/)?(www\.)?[a-z0-9\-\.]{3,}\.[a-z]{3}$/.test(value)
-}
 
 // Returns a link to a valid url prefixed with '?ref=' otherwise returns null
+import validUrl from 'valid-url'
 function getSourceUri (queryString) {
   const queryParam = '?ref='
   let source
   if (queryString.includes(queryParam)) {
     let sourceCandidate = queryString.replace(queryParam, '')
-    if (validUri(sourceCandidate)) {
+    if (validUrl.isUri(sourceCandidate)) {
       source = sourceCandidate
     }
   }
