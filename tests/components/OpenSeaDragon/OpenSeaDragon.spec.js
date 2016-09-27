7b0d6766b98f86db14167ed821decc6a9df10977
Dan Wolfe <dwolfe2@nd.edu>
2016-09-27 12:30:43 -0400

    Fix broken tests.

--- tests/components/OpenSeaDragon/OpenSeaDragon.spec.js
+++ tests/components/OpenSeaDragon/OpenSeaDragon.spec.js
@@ -2,6 +2,7 @@ import React from 'react'
 import OpenSeaDragon from 'components/OpenSeaDragon'
 import OpenSeaDragonControls from 'components/OpenSeaDragonControls'
 import OpenSeaDragonNavigator from 'components/OpenSeaDragonNavigator'
+import { CircularProgress } from 'material-ui'
 import { shallow } from 'enzyme'
 
 describe('(View) OpenSeaDragon', () => {
@@ -16,12 +17,17 @@ describe('(View) OpenSeaDragon', () => {
     expect(_component.props().image).to.be.defined
   })
 
-  it('Renders a <OpenSeaDragonControls/> component', () => {
-      expect(_component.find(OpenSeaDragonControls)).to.exist
+  it('Renders a <CircularProgress/> component before loading', () => {
+    expect(_component.find(CircularProgress)).to.exist
+  })
+  it('Renders a <OpenSeaDragonControls/> component after loading', () => {
+    _component.setState({loading: false})
+    expect(_component.find(OpenSeaDragonControls)).to.exist
   })
 
-  it('Renders a <OpenSeaDragonNavigator/> component', () => {
-      expect(_component.find(OpenSeaDragonNavigator)).to.exist
+  it('Renders a <OpenSeaDragonNavigator/> component after loading', () => {
+    _component.setState({loading: false})
+    expect(_component.find(OpenSeaDragonNavigator)).to.exist
   })
 
 })
