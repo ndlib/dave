5c3ed0bb0df02d61d2d175f9ebaa15d4b663b275
Dan Wolfe <dwolfe2@nd.edu>
2016-10-03 10:29:01 -0400

    Fix tests

--- tests/components/OpenSeaDragonControls/OpenSeaDragonControls.spec.js
+++ tests/components/OpenSeaDragonControls/OpenSeaDragonControls.spec.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import OpenSeaDragonControls from 'components/OpenSeaDragonControls'
 import OpenSeaDragonButton from 'components/OpenSeaDragonButton'
+import OpenSeaDragonResetButton from 'components/OpenSeaDragonResetButton'
 import { shallow } from 'enzyme'
 
 describe('(View) OpenSeaDragonControls', () => {
@@ -11,21 +12,22 @@ describe('(View) OpenSeaDragonControls', () => {
     expect(_component.find('ul')).to.have.lengthOf(1)
   })
 
-  it('Renders six <OpenSeaDragonButton/> components on desktop', () => {
+  it('Renders five <OpenSeaDragonButton/> and one <OpenSeaDragonResetButton /> component on desktop', () => {
     global.navigator = {
       userAgent: null
     };
     _component = shallow(<OpenSeaDragonControls />)
-    expect(_component.find(OpenSeaDragonButton)).to.have.lengthOf(6)
-
+    expect(_component.find(OpenSeaDragonButton)).to.have.lengthOf(5)
+    expect(_component.find(OpenSeaDragonResetButton)).to.have.lengthOf(1)
   })
 
-  it('Renders three <OpenSeaDragonButton/> components on mobile', () => {
+  it('Renders two <OpenSeaDragonButton/> and one <OpenSeaDragonResetButton /> component on mobile', () => {
     global.navigator = {
       userAgent: 'iPad'
     };
     _component = shallow(<OpenSeaDragonControls />)
-    expect(_component.find(OpenSeaDragonButton)).to.have.lengthOf(3)
+    expect(_component.find(OpenSeaDragonButton)).to.have.lengthOf(2)
+    expect(_component.find(OpenSeaDragonResetButton)).to.have.lengthOf(1)
   })
 
 
